a06808b69d8f77618003e8008d4dc943a5c0d87d
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-07-01 14:14:07 -0400

    added files

--- movies-search-app/src/App.js
+++ movies-search-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  state = {
+    baseURL: 'http://www.omdbapi.com/?',
+    apikey: 'apikey=65ddf0d7',
+    query: '&t=',
+    movieTitle: '',
+    searchURL: ''
+  }
+
+  handleChange =  (event) =>{
+    this.setState({[event.target.id]: event.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({searchURL:this.state.baseURL + this.state.apikey + this.state.query + this.state.movieTitle})
+  }
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Movie App</h1>
+
+
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='movieTitle'>Title</label>
+
+        <input type="text" id="movieTitle" 
+        value={this.state.movieTitle}
+        onChange={this.handleChange}
+        />
+
+        <input type="submit" /> 
+      </form>
+      <a href={this.state.searchURL}>{this.state.searchURL}</a>
     </div>
   );
 }
+}
 
-export default App;
+export default App
