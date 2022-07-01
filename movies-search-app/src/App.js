ca68eeb609b70e6054231dc9dcaf8d87ad05de8b
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-07-01 15:00:19 -0400

    added new api

--- movies-search-app/src/App.js
+++ movies-search-app/src/App.js
@@ -1,44 +1,55 @@
-import './App.css';
-import { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
+import MovieInfo from "./component/MovieInfo";
 
 class App extends Component {
   state = {
-    baseURL: 'http://www.omdbapi.com/?',
-    apikey: 'apikey=65ddf0d7',
-    query: '&t=',
-    movieTitle: '',
-    searchURL: ''
-  }
+    baseURL: "http://www.omdbapi.com/?",
+    apiKey: "apikey=65ddf0d7",
+    query: "&t=",
+    movieTitle: "",
+    searchURL: "",
+    movie: null
+  };
 
-  handleChange =  (event) =>{
-    this.setState({[event.target.id]: event.target.value})
-  }
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
 
   handleSubmit = (e) => {
-    e.preventDefault()
-    this.setState({searchURL:this.state.baseURL + this.state.apikey + this.state.query + this.state.movieTitle})
+    e.preventDefault();
+    this.setState({searchURL: this.state.baseURL + this.state.apiKey + this.state.query + this.state.movieTitle }, () => {
+      fetch(this.state.searchURL)
+      .then(res => res.json())
+      .then(json => this.setState({movie: json}))
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Movies APP</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="movieTitle">Title</label>
+
+          <input
+            type="text"
+            id="movieTitle"
+            value={this.state.movieTitle}
+            onChange={this.handleChange}
+          />
+
+          <input type="submit" />
+        </form>
+
+        <a href={this.state.searchURL}>{this.state.searchURL}</a>
+
+        {this.state.movie && <MovieInfo movie={this.state.movie}/>}
+      </div>
+    );
   }
-  
-  render(){
-  return (
-    <div className="App">
-      <h1> Movie App</h1>
-
-
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor='movieTitle'>Title</label>
-
-        <input type="text" id="movieTitle" 
-        value={this.state.movieTitle}
-        onChange={this.handleChange}
-        />
-
-        <input type="submit" /> 
-      </form>
-      <a href={this.state.searchURL}>{this.state.searchURL}</a>
-    </div>
-  );
-}
 }
 
-export default App
+export default App;
